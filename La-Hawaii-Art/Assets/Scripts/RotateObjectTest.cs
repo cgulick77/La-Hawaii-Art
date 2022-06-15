f5731498361b4883cfc81763a00acfda70907a18
Christian <cgulick@student.fitchburgstate.edu>
2022-06-14 23:15:54 -0400

    Updated Script

--- La-Hawaii-Art/Assets/Scripts/RotateObjectTest.cs
+++ La-Hawaii-Art/Assets/Scripts/RotateObjectTest.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Input
+using UnityEngine.InputSystem;
 
 public class RotateObjectTest : MonoBehaviour
 {
