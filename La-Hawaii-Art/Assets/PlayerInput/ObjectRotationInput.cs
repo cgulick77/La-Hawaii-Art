1bb27ed9d7eb28f7ee1771344521c2f863ab9634
Christian <cgulick@student.fitchburgstate.edu>
2022-06-20 23:32:03 -0400

    Updated Player controls

--- La-Hawaii-Art/Assets/PlayerInput/ObjectRotationInput.cs
+++ La-Hawaii-Art/Assets/PlayerInput/ObjectRotationInput.cs
@@ -24,7 +24,7 @@ public class @ObjectRotationInput : IInputActionCollection, IDisposable
                     ""id"": ""4a2331fb-4d74-42c7-bed2-fe3159aaa9ad"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
-                    ""interactions"": """"
+                    ""interactions"": ""Press""
                 },
                 {
                     ""name"": ""Rotate Verticle"",
@@ -59,6 +59,77 @@ public class @ObjectRotationInput : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""Player"",
+            ""id"": ""bed89d83-bcbe-4d23-b073-f0c4b17f759f"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""dc7ebca3-7e67-4d51-afa4-ad94608f2d53"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""4bdb0d96-bba1-4007-86bb-758fdea302fc"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a0792c67-5174-4e8c-befa-f43e05e0bb73"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""7bd9b3a3-e10d-46fa-8b45-92dfb9304893"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""f234117b-3876-46c9-b960-56a6f5557ba0"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""bf04bc4f-68cd-43b4-9817-932f7616f7b4"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -67,6 +138,9 @@ public class @ObjectRotationInput : IInputActionCollection, IDisposable
         m_ObjectRotation = asset.FindActionMap("Object Rotation", throwIfNotFound: true);
         m_ObjectRotation_RotateHorizontal = m_ObjectRotation.FindAction("Rotate Horizontal", throwIfNotFound: true);
         m_ObjectRotation_RotateVerticle = m_ObjectRotation.FindAction("Rotate Verticle", throwIfNotFound: true);
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -153,9 +227,46 @@ public class @ObjectRotationInput : IInputActionCollection, IDisposable
         }
     }
     public ObjectRotationActions @ObjectRotation => new ObjectRotationActions(this);
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    public struct PlayerActions
+    {
+        private @ObjectRotationInput m_Wrapper;
+        public PlayerActions(@ObjectRotationInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
     public interface IObjectRotationActions
     {
         void OnRotateHorizontal(InputAction.CallbackContext context);
         void OnRotateVerticle(InputAction.CallbackContext context);
+    }
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
     }
 }
--- La-Hawaii-Art/Assets/Scripts/RotateObjectTest.cs
+++ La-Hawaii-Art/Assets/Scripts/RotateObjectTest.cs
@@ -6,14 +6,44 @@ using UnityEngine.InputSystem;
 public class RotateObjectTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private ObjectRotationInput objectRotationInput;
+   private InputAction rotateHorizontal;
+   private InputAction rotateVertical;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Awake() {
+    
+    objectRotationInput = new ObjectRotationInput();
+
+   }
+
+   private void OnEnable() {
+    
+    // rotateHorizontal = objectRotationInput.ObjectRotation.RotateHorizontal;
+    // rotateHorizontal.Enable();
+
+    objectRotationInput.ObjectRotation.RotateHorizontal.performed += RotateH;
+    objectRotationInput.ObjectRotation.RotateHorizontal.Enable();
+
+    objectRotationInput.ObjectRotation.RotateVerticle.performed += RotateV;
+    objectRotationInput.ObjectRotation.RotateVerticle.Enable();
+
+
+   }
+
+   private void OnDisable() {
+    objectRotationInput.ObjectRotation.RotateHorizontal.Disable();
+    objectRotationInput.ObjectRotation.RotateVerticle.Disable();
+   }
+
+   private void RotateH(InputAction.CallbackContext obj)
+   {
+        Debug.Log("Rotating Horizontally");
+        transform.Rotate(0f,10.0f,0f);
+   }
+
+    private void RotateV(InputAction.CallbackContext obj)
+   {
+        Debug.Log("Rotating Vertically");
+         transform.Rotate(0f,0f,10f);
+   }
 }
